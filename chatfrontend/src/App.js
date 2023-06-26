a82c6ea7e830835c61eb11a4bfa45e376a4cfc57
santosh kyamanakeri <85493557+santoshkyamanakeri@users.noreply.github.com>
2023-06-26 16:34:58 +0530

    section-5
    creating auth components and authentication user

--- chatfrontend/src/App.js
+++ chatfrontend/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React from 'react';
+import Login from './Components/Auth/Login'
+import Register from './Components/Auth/Register'
+import Chat from './Components/Chat/Chat1'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import "./App.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chat App</h1>
-    </div>
+      <Router>
+          <div className="App">
+              <Switch>
+                  <Route exact path='/' component={Chat} />
+                  <Route path='/login' component={Login} />
+                  <Route path='/register' component={Register} />
+                  <Route render={() => <h1>404 page not found</h1>} />
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
